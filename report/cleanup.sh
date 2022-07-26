66f0beaaf7c3d6c54e840c777a529fe261ccbe6a
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-07-27 00:10:42 +0200

    fix

--- report/cleanup.sh
+++ report/cleanup.sh
@@ -5,4 +5,4 @@ cd "$SCRIPT_DIR" || exit
 rm -f main.pdf 2> /dev/null
 rm -f ./src/chapters/*.aux
 mv ./src/main.pdf . 2> /dev/null
-find ./src/ -type f -not -name '*.tex' -delete
+find ./src/ ! \( -name '∗.tex' -o -name '*.png' \) -exec rm "{}" \;
