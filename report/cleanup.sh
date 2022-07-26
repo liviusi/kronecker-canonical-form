8c7632469a9af6677d622652afbbd890f0413117
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-07-27 00:28:15 +0200

    fixed cleanup script

--- report/cleanup.sh
+++ report/cleanup.sh
@@ -5,4 +5,6 @@ cd "$SCRIPT_DIR" || exit
 rm -f main.pdf 2> /dev/null
 rm -f ./src/chapters/*.aux
 mv ./src/main.pdf . 2> /dev/null
-find ./src/ ! \( -name '∗.tex' -o -name '*.png' \) -exec rm "{}" \;
+cd ./src/ || exit
+GLOBIGNORE=*.tex
+rm -v * 2> /dev/null
