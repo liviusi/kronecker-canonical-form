e25e00a4eaec9ce54daa72c3020d2032f712fd61
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-07-26 23:48:12 +0200

    report structure

---
+++ report/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || exit
+rm -f main.pdf 2> /dev/null
+rm -f ./src/chapters/*.aux
+mv ./src/main.pdf . 2> /dev/null
+find ./src/ -type f -not -name '*.tex' -delete
