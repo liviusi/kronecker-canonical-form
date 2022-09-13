9fb5553a793d11e693598a44be24ad164732547f
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-09-13 20:31:49 +0200

    still has to be populated

--- tests/test_kcf.py
+++ tests/test_kcf.py
@@ -18,13 +18,3 @@ def test_L0_L0T():
             and not L.det().is_zero() and not R.det().is_zero()), (
         f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\n",
         f"Expected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}")
-
-
-def test_L2_N():
-    """
-    L_2; N
-    """
-    A = sa.matrix(sa.SR, [[2, 1, 3], [3, 2, 5], [3, 2, 6]])
-    B = sa.matrix(sa.SR, [[1, 1, 2], [1, 1, 2], [1, 1, 3]])
-    (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
-    assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
