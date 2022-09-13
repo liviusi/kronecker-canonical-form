d9a03c5abfafb1753264d504a2569066ad52a97a
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-09-13 20:26:55 +0200

    refactored tests

--- tests/test_kcf.py
+++ tests/test_kcf.py
@@ -3,47 +3,28 @@ import sage.all as sa
 from kcf import kcf_sage as kcf
 
 
-def test_one():
-    """
-    L_1; L_1^T
-    """
-    A = sa.matrix(sa.SR, [[0, 1, 0], [0, 0, 0], [0, 0, 1]])
-    B = sa.matrix(sa.SR, [[1, 0, 0], [0, 0, 1], [0, 0, 0]])
-    (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
-    assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
 
-def test_two():
-    """
-    L_0; L_0^T
-    """
-    A = sa.matrix(sa.SR, [[0]])
-    B = sa.matrix(sa.SR, [[1]])
-    (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
-    assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
-
-def test_three():
-    """
-	Edge case (kind of).
-    """
+def test_L0_L0T():
     A = sa.matrix(sa.SR, [[1]])
     B = sa.matrix(sa.SR, [[0]])
+    while True:
+        P = sa.random_matrix(sa.ZZ, *A.dimensions()).change_ring(sa.SR)
+        if not (P.det().is_zero()):
+            break
+    A = P.inverse() * A * P
+    B = P.inverse() * B * P
     (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
-    assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
+    assert ((L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero()
+            and not L.det().is_zero() and not R.det().is_zero()), (
+        f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\n",
+        f"Expected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}")
 
-def test_four():
+
+def test_L2_N():
     """
     L_2; N
     """
     A = sa.matrix(sa.SR, [[2, 1, 3], [3, 2, 5], [3, 2, 6]])
     B = sa.matrix(sa.SR, [[1, 1, 2], [1, 1, 2], [1, 1, 3]])
-    (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
-    assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
-
-def test_five():
-    """
-	L_2; L_2
-    """
-    A = sa.matrix(sa.SR, [[0, 3, -1, 4], [2, -108, 0, 1], [-2, -1, -1, 1], [4, 1, -4, 1]])
-    B = sa.matrix(sa.SR, [[0, 0, 0, -1], [0, 2, 0, 0], [0, -2, 0, -1], [0, 4, 0, -4]])
     (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
     assert (L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero(), f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\nExpected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}"
