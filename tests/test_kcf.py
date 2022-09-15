2f4b52ac8606e6933f4d0ae1d7bbddad2d3accca
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-09-15 15:09:46 +0200

    new test

--- tests/test_kcf.py
+++ tests/test_kcf.py
@@ -169,3 +169,31 @@ def test_L3T_N2_J2():
             and not L.det().is_zero() and not R.det().is_zero()), (
         f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\n",
         f"Expected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}")
+
+
+def test_L3_N3_J2_F3():
+    L3_A = sa.matrix(sa.SR, [[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    L3_B = sa.matrix(sa.SR, [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    A = sa.block_diagonal_matrix([L3_A,
+                                  sa.identity_matrix(2),
+                                  sa.matrix(sa.SR, [[42, 1],
+                                                    [0, 42]])]).transpose()
+    B = sa.block_diagonal_matrix([L3_B,
+                                  sa.matrix(sa.SR, [[0, 1], [0, 0]]),
+                                  sa.identity_matrix(2)]).transpose()
+    while True:
+        D = sa.random_matrix(sa.ZZ, A.nrows(), A.nrows()).change_ring(sa.SR)
+        if not (D.det().is_zero()):
+            while True:
+                C = sa.random_matrix(sa.ZZ, A.ncols(),
+                                     A.ncols()).change_ring(sa.SR)
+                if not (C.is_zero()):
+                    break
+            break
+    A = D.inverse() * A * C
+    B = D.inverse() * B * C
+    (L, R), (KCF_A, KCF_B) = kcf.kronecker_canonical_form(A, B, True)
+    assert ((L*A*R - KCF_A).is_zero() and (L*B*R - KCF_B).is_zero()
+            and not L.det().is_zero() and not R.det().is_zero()), (
+        f"Gotten:\n{kcf.stringify_pencil(KCF_A, KCF_B)}\n",
+        f"Expected:\n{kcf.stringify_pencil(L*A*R, L*B*R)}")
