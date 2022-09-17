9df92d424235f5093a4d67c04a0d87f7d1e4f246
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-09-17 15:38:44 +0200

    rename

--- kcf/profiling.py
+++ kcf/profiling.py
@@ -5,24 +5,24 @@ import cProfile
 import pstats
 
 
-def random_pencil(m: int, n: int) -> tuple:
+def _random_pencil(m: int, n: int) -> tuple:
     return (sa.random_matrix(sa.ZZ, m, n).change_ring(sa.SR),
             sa.random_matrix(sa.ZZ, m, n).change_ring(sa.SR))
 
 
-def run() -> tuple[int, int, pstats.Stats]:
+def _run() -> tuple[int, int, pstats.Stats]:
     pr = cProfile.Profile()
     m = randint(1, 20)
     n = randint(1, 20)
     pr.enable()
-    kcf.kronecker_canonical_form(*random_pencil(m, n))
+    kcf.kronecker_canonical_form(*_random_pencil(m, n))
     pr.disable()
     return m, n, pstats.Stats(pr)
 
 
 def main() -> None:
     for _ in range(10):
-        m, n, s = run()
+        m, n, s = _run()
         print(f'Dimensions: ({m}, {n})')
         s.print_stats('kcf')
 
